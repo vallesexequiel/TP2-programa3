996709915f3f46b5b8d3f7fb440e58159df3533e
valles exequiel matias <exequielvalles21@gmai.com>
2025-05-23 18:01:40 -0300

    creamos la funcion de calculadora y su resultado da en un alert

---
+++ ejercicio1/ejercicio4.js
@@ -0,0 +1,11 @@
+function calcularIMC() {
+  const estatura = parseFloat(document.getElementById('estatura').value);
+  const peso = parseFloat(document.getElementById('peso').value);
+
+  if (estatura > 0 && peso > 0) {
+    const imc = peso / (estatura * estatura);
+    alert(`Tu IMC es: ${imc.toFixed(2)}`);
+  } else {
+    alert('Por favor, ingresa valores válidos.');
+  }
+}
